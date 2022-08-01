04e0df983ce1624ba9971180fefed4d8268bdefe
Martin <martinjxm@gmail.com>
2022-08-01 02:14:40 -0400

    removed content

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,4 @@
 import Head from "next/head";
-import Image from "next/image";
-import Countdown from "../components/Countdown";
-import us from "../public/mollyandmarty.jpg";
 
 export default function Home() {
   return (
@@ -14,25 +11,11 @@ export default function Home() {
         />
         <meta name="robots" content="index, follow" />
         <link rel="canonical" href="https://mollyandmarty.party" />
-        <link rel="icon" href="image/png" href="/favicon.png" />
-        <link
-          rel="icon"
-          type="image/png"
-          href="https://mollyandmarty.party/favicon.png"
-        />
+
       </Head>
 
       <main className="flex flex-col items-center justify-center w-full flex-1 px-20 text-center">
-        <h1 className="text-6xl font-bold p-12">
-          Molly and Marty's Celebrations
-        </h1>
-
-        <h2 className="text-5xl font-bold p-5">Wedding</h2>
-        <Countdown date="10/15/22" />
       </main>
-      <div className="mr-10">
-        <Image src={us} alt="Molly and Marty" width="480" height="640" />
-      </div>
     </div>
   );
 }
